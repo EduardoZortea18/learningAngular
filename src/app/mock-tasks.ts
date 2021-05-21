a4b4a034066c1a3b88079df478d836e315fb05ad
EduardoZortea18 <60518656+EduardoZortea18@users.noreply.github.com>
2021-05-20 21:31:36 -0300

    a

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,31 @@
+import {Task} from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Homework',
+    day: 'May 5th',
+    reminder: false
+  },
+  {
+    id: 2,
+    text: 'Doctor',
+    day: 'May 7th',
+    reminder: true
+  },{
+    id: 3,
+    text: 'Job Meeting',
+    day: 'May 4th',
+    reminder: true
+  },{
+    id: 4,
+    text: 'Lunch w/ family',
+    day: 'May 6th',
+    reminder: true
+  },{
+    id: 5,
+    text: 'Study',
+    day: 'May 8th',
+    reminder: true
+  }
+]
